8dd15dec9a7c7ed2a30fa555bde518c5d5b50db8
rollrat <rollrat.cse@gmail.com>
2022-05-13 17:12:04 +0900

    test(user): add user login failure code

--- src/test/java/candy/server/service/UserServiceTest.java
+++ src/test/java/candy/server/service/UserServiceTest.java
@@ -42,4 +42,16 @@ public class UserServiceTest {
         userService.join(signup);
         assertThatIllegalArgumentException().isThrownBy(() -> userService.join(signup));
     }
+
+    @Test
+    void 유저_로그인_실패() {
+        UserDto.Login login = UserDto.Login
+                .builder()
+                .id("testid")
+                .pw("testpw")
+                .build();
+
+        // 유저가 없어야하기 때문에 실패함.
+        assertThatIllegalArgumentException().isThrownBy(() -> userService.login(login));
+    }
 }
