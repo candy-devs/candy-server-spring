c3f317327233c372f974d8e7eb7c05ba6656926e
rollrat <rollrat.cse@gmail.com>
2022-06-03 20:44:06 +0900

    security: add security account domain (for test)

--- src/main/java/candy/server/SpringConfig.java
+++ src/main/java/candy/server/SpringConfig.java
@@ -4,6 +4,8 @@ import candy.server.domains.user.repository.UserRepository;
 import candy.server.domains.user.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SpringConfig {
@@ -16,5 +18,11 @@ public class SpringConfig {
     @Bean
     public UserService userService() {
         return new UserService(userRepository);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+//    return NoOpPasswordEncoder.getInstance();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
---
+++ src/main/java/candy/server/domains/security/AccountService.java
@@ -0,0 +1,36 @@
+package candy.server.domains.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService implements UserDetailsService {
+    private AccountRepository accountRepository;
+
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = accountRepository.findByUsername(username);
+        if (account == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return User.builder()
+                .username(account.getUsername())
+                .password(account.getPassword())
+                .roles(account.getRole())
+                .build();
+    }
+
+    public Account createNew(Account account) {
+        account.encodePassword(passwordEncoder);
+        return accountRepository.save(account);
+    }
+
+}
