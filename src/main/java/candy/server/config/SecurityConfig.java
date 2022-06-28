f8e002d60849537fe95975614f9b2b45595d1a60
rollrat <rollrat.cse@gmail.com>
2022-06-28 17:16:29 +0900

    build(security): /admin은 ROLE_ADMIN만 접근 가능하도록 변경

--- src/main/java/candy/server/config/SecurityConfig.java
+++ src/main/java/candy/server/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/", "/**").permitAll()
+                    .antMatchers("/v1/", "/v1/**").permitAll()
                     .antMatchers("/admin/**").hasRole(UserRoleEnum.ADMIN.name())
                     .anyRequest().authenticated()
                     .accessDecisionManager(accessDecisionManager())
