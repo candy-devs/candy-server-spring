3d86927c496ec16a970d23aa28c73009b1e18c8d
rollrat <rollrat.cse@gmail.com>
2022-07-12 12:47:11 +0900

    chore(article): imm article interfactor info에 equals, hashCode 추가

---
+++ src/main/java/candy/server/domain/article/entity/ImmArticleInteractionInfo.java
@@ -0,0 +1,34 @@
+package candy.server.domain.article.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+@Getter
+@Builder
+public class ImmArticleInteractionInfo {
+    private int articleCommentCount;
+    private int articleNotice;
+    private int articleView;
+    private int articleUpvote;
+    private int articleDownvote;
+    private int articleBookmarkCount;
+
+    protected ImmArticleInteractionInfo() {}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ImmArticleInteractionInfo that = (ImmArticleInteractionInfo) o;
+        return articleCommentCount == that.articleCommentCount && articleNotice == that.articleNotice && articleView == that.articleView && articleUpvote == that.articleUpvote && articleDownvote == that.articleDownvote && articleBookmarkCount == that.articleBookmarkCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(articleCommentCount, articleNotice, articleView, articleUpvote, articleDownvote, articleBookmarkCount);
+    }
+}
