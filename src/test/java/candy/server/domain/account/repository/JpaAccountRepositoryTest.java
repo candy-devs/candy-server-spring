a198f15fa7a029da599ea9e9398f070ec5529059
JJONSOO <pjs970616@naver.com>
2022-07-21 20:35:49 +0900

    account test

---
+++ src/test/java/candy/server/domain/account/repository/JpaAccountRepositoryTest.java
@@ -0,0 +1,64 @@
+package candy.server.domain.account.repository;
+
+import candy.server.domain.account.dao.JpaAccountRepository;
+import candy.server.domain.account.dto.AccountSendRequestDto;
+import candy.server.domain.account.entity.CaAccountEntity;
+import candy.server.domain.account.service.AccountSendService;
+import candy.server.domain.article.dao.JpaArticleRepository;
+import candy.server.domain.article.entity.CaArticleEntity;
+import candy.server.domain.bookmark.entity.CaArticleBookmarkEntity;
+import candy.server.domain.user.dao.JpaUserRepository;
+import candy.server.domain.user.entity.CaUserEntity;
+import candy.server.domain.user.entity.UserRoleEnum;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import java.sql.Timestamp;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class JpaAccountRepositoryTest {
+
+    @Autowired
+    private JpaUserRepository userRepository;
+    @Autowired
+    private JpaAccountRepository accountRepository;
+
+    CaUserEntity targetUser;
+    @BeforeEach
+    void before(){
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        targetUser = CaUserEntity.UserBuilder()
+                .userIdid("test_id")
+                .userPw("testpw")
+                .userNickname("testnickname")
+                .userSignupTime(timestamp)
+                .userLastLoginTime(timestamp)
+                .userRole(UserRoleEnum.USER)
+                .build();
+        userRepository.save(targetUser);
+    }
+
+
+
+    @Test
+    void findByUserId(){
+        CaAccountEntity caAccountEntity =CaAccountEntity.builder()
+                .accountId(targetUser.getUserId())
+                .caUserEntity(targetUser)
+                .accountUserCandyCnt(3L)
+                .build();
+        accountRepository.saveAndFlush(caAccountEntity);
+
+        CaAccountEntity caAccountEntity1 = accountRepository.findByAccountId(targetUser.getUserId()).get();
+        System.out.println(caAccountEntity1.getAccountUserCandyCnt());
+        Assertions.assertThat(caAccountEntity1).isNotNull();
+    }
+
+}
