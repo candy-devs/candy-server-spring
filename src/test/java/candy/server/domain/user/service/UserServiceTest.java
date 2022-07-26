70d754c0ff00a804c38b5594652c7a9b9d07c032
JJONSOO <pjs970616@naver.com>
2022-07-26 15:57:55 +0900

    profile commentcount , articlecount , accountcount

--- src/test/java/candy/server/domain/user/service/UserServiceTest.java
+++ src/test/java/candy/server/domain/user/service/UserServiceTest.java
@@ -1,10 +1,15 @@
 package candy.server.domain.user.service;
 
+import candy.server.domain.account.dao.JpaAccountRepository;
+import candy.server.domain.account.entity.CaAccountEntity;
 import candy.server.domain.board.dao.JpaBoardRepository;
 import candy.server.domain.board.entity.CaBoardEntity;
+import candy.server.domain.user.dao.JpaUserProfileRepository;
 import candy.server.domain.user.dao.JpaUserRepository;
 import candy.server.domain.user.dto.UserModifyRequestDto;
+import candy.server.domain.user.dto.UserProfileRequestDto;
 import candy.server.domain.user.entity.CaUserEntity;
+import candy.server.domain.user.entity.CaUserProfileEntity;
 import candy.server.domain.user.entity.UserRoleEnum;
 import candy.server.security.model.SessionUser;
 import org.assertj.core.api.Assertions;
@@ -28,15 +33,20 @@ public class UserServiceTest {
     private UserService userService;
     @Autowired
     private JpaUserRepository userRepository;
-
+    @Autowired
+    private JpaAccountRepository accountRepository;
+    @Autowired
+    private JpaUserProfileRepository userProfileRepository;
+    private CaUserEntity caUserEntity;
     @BeforeEach
     void 테스트_유저_생성() {
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        CaUserEntity caUserEntity = CaUserEntity.UserBuilder()
+        caUserEntity = CaUserEntity.UserBuilder()
                 .userIdid("testid")
                 .userPw("testpw")
                 .userNickname("testnickname")
                 .userSignupTime(timestamp)
+                .userSpecificId("1234")
                 .userLastLoginTime(timestamp)
                 .userRole(UserRoleEnum.USER)
                 .build();
@@ -54,5 +64,18 @@ public class UserServiceTest {
                 .build());
 
         Assertions.assertThat(userEntity.get().getUserNickname()).isEqualTo("testnickname-modify");
+    }
+
+    @Test
+    void test_Profile() {
+//        Optional<CaUserProfileEntity> user = userProfileRepository.findByUserId_UserNickname("testnickname");
+        userProfileRepository.save(CaUserProfileEntity.builder().userId(caUserEntity).writeArticleCount(3L).writeCommentCount(3L).build());
+        CaAccountEntity accountEntity = CaAccountEntity.builder()
+                .caUserEntity(caUserEntity)
+                .accountCount(3L)
+                .build();
+        accountRepository.save(accountEntity);
+        UserProfileRequestDto userProfileRequestDto = userService.getuserProfileRequestDto("1234");
+        Assertions.assertThat(userProfileRequestDto.getAccountCount()).isEqualTo(3L);
     }
 }
