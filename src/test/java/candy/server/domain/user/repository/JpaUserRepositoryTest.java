9fddfbb51e8e8f6ba1153ef3c89058b7997abf4b
JJONSOO <pjs970616@naver.com>
2022-07-26 19:53:44 +0900

    Change user Domain

--- src/test/java/candy/server/domain/user/repository/JpaUserRepositoryTest.java
+++ src/test/java/candy/server/domain/user/repository/JpaUserRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Timestamp;
